c9ce8628eb8a6699d656ae8e4830c62670219541
fireflyoo <lufeng369@gmail.com>
2016-07-27 09:15:11 +0800

    Add files via upload

--- REPL.js
+++ REPL.js
@@ -1,96 +1,57 @@
 var REPL = {
-	    version: "0.3",
-		executing: true,
-		maximumInspectionDepth: 100,
-	    changeLog: {
+	       version: "0.3",
+	       changeLog: {
 			"0.3":["added help message"],
 			"0.2":["Wrapped REPL inside of a try/catch block.",
-				   "Added multi-line mode.",
-				   "Added Object Inspector",
-				   "Added Include File Mechanism"],
+                               "Added multi-line mode."],
 			"0.1":["Initial Release"]
 		},
 	
 		start: function() {
 			REPL.showMOTD();
-			REPL.executing = true;
 			REPL.exec();
 		},
 	
 		showMOTD: function() {
 			WScript.StdOut.WriteLine ("Welcome to the WSH REPL V "+REPL.version);
 			WScript.StdOut.WriteLine ("Granting Wishes!");
-			WScript.StdOut.WriteLine ("use REPL.inspect(REPL) for help.");
-		},
-	
-		getPrompt: function () {
-			return REPL.prompt;
-		},
-	
-		include: function(jsFile) {
-			var fso = new ActiveXObject("Scripting.FileSystemObject");
-			var f = fso.OpenTextFile(jsFile);
-			var s = f.ReadAll();
-			f.Close();
-			return (eval(CoffeeScript.compile(s,{"bare":true})));
+			WScript.StdOut.WriteLine ("use say for print.");
+                        WScript.StdOut.WriteLine ("use _ to see the latest value \nand _$ for the compiled js source;");
 		},
 	
 	
 		exec: function() {
-      	var isMultiLineMode=false;
-        var cmd="";
-        var singleLine="";
-        var multiLine="";
-        var prompt = "Coffee> ";
-        var blankPrompt = "      >";
-			while(REPL.executing) {
-				WScript.StdOut.Write(isMultiLineMode?blankPrompt:prompt);
-
-				singleLine = WScript.StdIn.ReadLine();
+      	               var _isMultiLinesMode=false;
+                       var _singleLine="";
+                       var _multiLines="";
+                       var _; 
+                       var _$; 
+					   var __$;
+                       var say = function (args) {WScript.StdOut.WriteLine(args);};
+                       while(true) {
+                            WScript.StdOut.Write(_isMultiLinesMode?"      >":"Coffee>");
+                            _$=__$;
+                            _singleLine = WScript.StdIn.ReadLine();
  
-	      isMultiLineMode=!singleLine?!isMultiLineMode:isMultiLineMode;
+                            if(!_singleLine)_isMultiLinesMode=!_isMultiLinesMode;
         
         
-				if (isMultiLineMode) {
-						multiLine += singleLine + "\n";
-				}
+                            if (_isMultiLinesMode) {
+        	            _multiLines += _singleLine + "\n";
+                     }
 	
-      else {
-            cmd= multiLine || singleLine;
-            multiLine="";
-					try {
-						WScript.StdOut.WriteLine(eval(CoffeeScript.compile(cmd,{"bare":true})));
-					} catch(e) {
-						REPL.displayError(e);
-					}
-				}
+                     else {
+                            _singleLine = _multiLines || _singleLine;
+                            _multiLines="";
+                            try {
+                                  __$=CoffeeScript.compile(_singleLine,{"bare":true});
+                                  _=eval(__$);
+                                  say(_);
+                            }      catch(e) {
+                                  REPL.displayError(e);
+                                   }
+                     }
 			}
-		},
-	
-		inspectionDepth: 0,
-		inspect: function(obj) {
-			if (REPL.inspectionDepth > REPL.maximumInspectionDepth) {
-				WScript.StdOut.WriteLine ("Maximum Inspection Depth Achived.  Good job!");
-				return;
-			}
-	
-			var depthStr = "";
-			for (var i = 1; i <= REPL.inspectionDepth; i++) {
-				depthStr = depthStr + "  ";
-			}
-	
-			REPL.inspectionDepth = REPL.inspectionDepth + 1;
-			if ((typeof obj) == "object") {
-				WScript.StdOut.WriteLine("Object");
-				for (var prop in obj) {
-					WScript.StdOut.Write(depthStr + prop +" : ");
-					REPL.inspect(obj[prop]);
-				}
-			} else {
-				WScript.StdOut.Write("("+(typeof obj)+ ") "+obj);
-			}
-			WScript.StdOut.WriteLine("");
-			REPL.inspectionDepth = REPL.inspectionDepth - 1;
 		},
 	
 		displayError: function(e) {
@@ -98,21 +59,5 @@ var REPL = {
 			WScript.StdOut.WriteLine(e + " " + e.number);
 			WScript.StdOut.WriteLine(e.name);
 			WScript.StdOut.WriteLine(e.description);
-		},
-	
-		info: function() {
-			WScript.StdOut.WriteLine("WSH REPL Version "+REPL.version);
-			WScript.StdOut.WriteLine("With Super Genie Powers!");
-			WScript.StdOut.WriteLine("");
-			WScript.StdOut.WriteLine("Change Log");
-			WScript.StdOut.WriteLine("----------");
-			REPL.inspect(REPL.changeLog);
-		},
-	
-		stop: function() {
-			REPL.executing = false;
-		},
-	
-		quit: function() { REPL.stop(); },
-		exit: function() { REPL.stop(); }
+		}
 	}
